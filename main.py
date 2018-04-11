4ed258b8659e62b4d96936f85e2f33e35b5a68af
jcdfusion <jaime.cortes@upr.edu>
2018-04-11 11:42:53 -0400

    update

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-from flask import jsonify
 from flask import Flask
-from flask_pymongo import PyMongo
-import pymongo
-import certifi
-import ssl
-from pymongo import MongoClient
-
-
-
-
 
 app = Flask(__name__)
-mongo = PyMongo(app)
-
-client = pymongo.MongoClient('mongodb+srv://devUser:AAG2018@cluster0-tomfu.mongodb.net/AutoAirGroupdb',ssl_cert_reqs=ssl.CERT_REQUIRED, ssl_ca_certs=certifi.where())
-
-print(client.AutoAirGroupdb)
-
-
 
 @app.route('/')
 def test():
