fe1bdb21099013b1d751b9949c3cdb8f4155a417
Luis <luis@luis.perez67@upr.edu>
2018-04-11 11:36:00 -0400

    update

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 import pymongo
 import certifi
 import ssl
-
+x=1
 
 client = pymongo.MongoClient('mongodb+srv://devUser:AAG2018@cluster0-tomfu.mongodb.net/AutoAirGroupdb',ssl_cert_reqs=ssl.CERT_REQUIRED, ssl_ca_certs=certifi.where())
 
