d83b795fdbaf5a60c8514a4d49cc9e3f48cbefc3
jcdfusion <jaime.cortes@upr.edu>
2018-04-12 09:52:53 -0400

    update

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from config.dbconfig import client
 
-
-
 app = Flask(__name__)
 #mongo = PyMongo(app)
 
 db = client.AutoAirGroupdb
 for doc in db.orders.find():
     print(doc)
-
-
 
 @app.route('/')
 def test():
