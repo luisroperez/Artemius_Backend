8964bf1d370f98ebbfcdc5ac7b2724a799b59ecd
luisroperez <luis.perez67@upr.edu>
2018-04-11 23:08:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from flask import jsonify
 from flask import Flask
+from flask_pymongo import PyMongo
+import pymongo
+import certifi
+import ssl
+from config.config import client
+from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+
+db = client.AutoAirGroupdb
+for doc in db.orders.find():
+    print(doc)
+
+
 
 @app.route('/')
 def test():
